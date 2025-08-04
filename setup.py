f684e20a8e8a39c31065de950c0f3064db455934
Stolniceanu Denisa <stolniceanudenisa@yahoo.com>
2025-08-04 10:43:29 +0300

    folder strucure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot - Generative AI',
+    version= '0.0.0',
+    author= 'Stolniceanu Denisa',
+    author_email= 'stolniceanudenisa@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
